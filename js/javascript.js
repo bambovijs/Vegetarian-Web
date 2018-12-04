8c098e40cf211cae71908c8181bb6e398d870788
Bambovijs <raivo@bambis.lv>
2018-12-04 17:12:31 +0200

    last update

--- js/javascript.js
+++ js/javascript.js
@@ -2,11 +2,13 @@
 $( document ).ready(function() {
     $(".menu").click(function() {
       $(".menu").addClass("active");
+      $(".triangle").addClass("active");
       $(".menu-open").addClass("active");
     })
 
     $(".close").click(function() {
       $(".menu-open").removeClass("active");
       $(".menu").removeClass("active");
+      $(".triangle").removeClass("active");
     })
 });
